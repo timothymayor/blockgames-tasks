11629d7b7af634168f03726bb69dd535c57acf59
Timothy Mayor <tim@timothymayor.net>
2022-03-19 21:45:00 +0100

    initial commit for simple_user_UI_Frontend.sol

---
+++ simple_user_UI_Frontend.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.10 and less than 0.9.0
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
+
+//updates coming on very soon
